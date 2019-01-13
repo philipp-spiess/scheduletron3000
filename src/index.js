1304010a9ccb7d5c6da0900cf5580be90a60829a
Philipp Spiess <hello@philippspiess.com>
2019-01-13 16:02:46 +0100

    Sync

--- src/index.js
+++ src/index.js
@@ -1,12 +1,127 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { NamePlates, names } from "./utils";
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {
+  unstable_runWithPriority,
+  unstable_scheduleCallback,
+  unstable_ImmediatePriority,
+  unstable_NormalPriority
+} from "scheduler";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import "./styles.css";
+
+class App extends React.Component {
+  state = {
+    filterValue: ""
+  };
+
+  handleChange = value => {
+    this.setState({ filterValue: value });
+  };
+
+  render() {
+    const { filterValue } = this.state;
+
+    return (
+      <div className="App">
+        <h1>ScheduleTron 3000</h1>
+
+        <FilterBox onChange={this.handleChange} />
+        <NamePlates names={names} filterValue={filterValue} />
+      </div>
+    );
+  }
+}
+
+class FilterBox extends React.Component {
+  state = {
+    inputValue: ""
+  };
+
+  handleChange = event => {
+    const value = event.target.value;
+
+    this.setState({ inputValue: value });
+    this.props.onChange(value);
+  };
+
+  render() {
+    const { inputValue } = this.state;
+
+    return (
+      <div className="clearfix">
+        <input
+          type="text"
+          className="input"
+          value={inputValue}
+          onChange={this.handleChange}
+          placeholder="Filter"
+        />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.render(<App />, rootElement);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+unstable_runWithPriority(unstable_ImmediatePriority, () => {
+  unstable_scheduleCallback(() => {
+    this.setState({ inputValue: value });
+  });
+});
+
+unstable_runWithPriority(unstable_NormalPriority, () => {
+  unstable_scheduleCallback(() => {
+    this.props.onChange(value);
+  });
+});
+
+
+
+const root = ReactDOM.render(
+  <React.unstable_ConcurrentMode>
+    <App />
+  </React.unstable_ConcurrentMode>,
+  rootElement
+);
+
+
+
+
+*/
---
+++ src/utils.js
@@ -0,0 +1,158 @@
+import React from "react";
+
+export const names = [
+  "Ada Moreno",
+  "Ada Stewart",
+  "Adele Valdez",
+  "Adeline Knight",
+  "Agnes Newman",
+  "Alfred Stephens",
+  "Allie Harris",
+  "Andre Owen",
+  "Andrew Clark",
+  "Angel Murray",
+  "Arthur Osborne",
+  "Arthur Woods",
+  "Barbara Holt",
+  "Billy Baker",
+  "Brian Vaughn",
+  "Calvin Edwards",
+  "Carl Pierce",
+  "Charles Sanchez",
+  "Charlie Hicks",
+  "Charlie Matthews",
+  "Chase Ruiz",
+  "Chester Chandler",
+  "Christopher Marsh",
+  "Cordelia Brock",
+  "Corey Gonzalez",
+  "Cornelia Gordon",
+  "Cory Lane",
+  "Daisy Pena",
+  "Dan Abramov",
+  "Dollie Wilkerson",
+  "Dominic Gannaway",
+  "Dorothy Singleton",
+  "Eddie Horton",
+  "Elmer Allen",
+  "Elnora Bass",
+  "Elnora Nichols",
+  "Esther Byrd",
+  "Esther Delgado",
+  "Ethan Hicks",
+  "Etta Wells",
+  "Evelyn Davidson",
+  "Frank Holloway",
+  "Fred Reed",
+  "Frederick Cunningham",
+  "Frederick Welch",
+  "Gene McKenzie",
+  "Georgia Montgomery",
+  "Gerald Ballard",
+  "Gordon Lewis",
+  "Gordon Wilson",
+  "Harriett Fletcher",
+  "Helen Love",
+  "Helen Weaver",
+  "Helena Cunningham",
+  "Howard King",
+  "Ian Rhodes",
+  "Inez Holmes",
+  "Isabel Owens",
+  "Jared McDaniel",
+  "Jason Anderson",
+  "Jeffrey Hardy",
+  "Jim Graham",
+  "Jordan Sparks",
+  "Jordan Wilkins",
+  "Katharine Stewart",
+  "Katherine Schwartz",
+  "Kathryn Patton",
+  "Keith Cross",
+  "Lena McGuire",
+  "Leo Hines",
+  "Lewis Barton",
+  "Lewis Webster",
+  "Lizzie Peters",
+  "Lois Bishop",
+  "Lottie Harper",
+  "Louis McBride",
+  "Mabel Daniels",
+  "Mabelle Bowman",
+  "Martha Moreno",
+  "Martin Simpson",
+  "Mary Powers",
+  "Mary Snyder",
+  "Matthew Collier",
+  "Maurice Myers",
+  "Myra Jennings",
+  "Myra Stephens",
+  "Nellie Potter",
+  "Nora Cannon",
+  "Ola Bryant",
+  "Paul Coleman",
+  "Pearl Moss",
+  "Phoebe Underwood",
+  "Rachel May",
+  "Sebastian Markbåge",
+  "Shawn Lloyd",
+  "Shawn Lynch",
+  "Sophie Alpert",
+  "Stella Lane",
+  "Steve Byrd",
+  "Steve Schmidt",
+  "Sunil Pai",
+  "Teresa Doyle",
+  "Timothy Gray",
+  "Timothy Harvey",
+  "Todd Stewart",
+  "Todd Washington",
+  "Willie Pope"
+];
+
+export function NamePlates({ names, filterValue }) {
+  return (
+    <div className="clearfix">
+      {names.map(name => (
+        <NamePlate
+          name={name}
+          filterValue={filterValue}
+          key={name}
+        />
+      ))}
+    </div>
+  );
+}
+
+export function NamePlate({ name, filterValue }) {
+  const matchesFilterAtIndex = name
+    .toLowerCase()
+    .indexOf(filterValue.toLowerCase());
+
+  sleep(3);
+
+  if (matchesFilterAtIndex >= 0) {
+    return (
+      <div className="nameplate">
+        {name.substring(0, matchesFilterAtIndex)}
+        <span className="highlight">
+          {name.substring(
+            matchesFilterAtIndex,
+            matchesFilterAtIndex + filterValue.length
+          )}
+        </span>
+
+        {name.substring(filterValue.length)}
+      </div>
+    );
+  } else {
+    return <div className="nameplate">{name}</div>;
+  }
+}
+
+export function sleep(ms) {
+  let now = Date.now();
+  while (Date.now() < now + ms) {
+    // noop
+  }
+}
